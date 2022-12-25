a20c4e9246d99c2a58d0a63d70f9c0ae1a5ed1ee
Shiguko <tllkloesh@gmail.com>
2022-12-25 15:40:07 +0300

    4

--- da/da.py
+++ da/da.py
@@ -1,5 +1,63 @@
-print('da')
-print("dnjnjsjdj")
-a=float(23)
-b=a+a
-print(b)
+# print('da')
+# print("dnjnjsjdj")
+# a=float(23)
+# b=a+a
+# print(b)
+
+
+# print(5/2)
+# print(5//2)
+# print(5%2)
+
+
+# a=5
+# print(a)
+# a=a+1 #full
+# print(a)
+# a+=1 #short
+# print(a)
+
+
+# a=input("ввести число:  ")
+# print(a)
+
+
+# a=input("слагаемое 1:  ")
+# b=input("слагаемое 2:  ")
+# c=a+b
+# print(c)
+
+
+# name = input("Please, enter your name  ")
+# surname=input('Enter your surname   ')
+# age =int(input('How old are you?   '))
+# print('Hello', name, surname)
+# print('Happy B-day')
+# age=age+1
+# print('You are',age,'years old')
+
+
+# print(5 == 2)
+# print(5 != 2)
+
+
+# a = int(input("слагаемое 1:  "))
+# b = int(input("слагаемое 2:  "))
+# c = a+b
+# print(c)
+
+
+# a=float(input("слагаемое 1:  "))
+# b=float(input("слагаемое 2:  "))
+# c=a+b
+# print(c)
+
+
+# h = int(input('Кол-во метров:   '))
+# r = int(input('Расход энергии на 1 метр:   '))
+# b = (h*r+100)*21
+# print(b)
+
+
+fraza = str(input('фраза:  '))
+print(fraza, fraza, fraza)
